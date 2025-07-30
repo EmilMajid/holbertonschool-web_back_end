65b6ca1005e279bcb257cd670c9ae10ce324d307
EmilMajid <emilinpoctu@gmail.com>
2025-07-30 20:06:27 +0400

    12

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module contains a function that returns a list of tuples with each element and its length.
+This module contains a function that returns a list
+of tuples with each element and its length.
 """
 
 from typing import (
