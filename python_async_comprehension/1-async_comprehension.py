bd8c28442935108174da1b9b1d8654535f62db03
EmilMajid <emilinpoctu@gmail.com>
2025-08-01 21:38:04 +0400

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+1-async_comprehension.py
+
+Defines an asynchronous coroutine that collects random numbers
+from an async generator using async comprehensions.
+"""
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
