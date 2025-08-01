2d4b2433eb9b5d611ec38d86a073c6eb2f686fa6
EmilMajid <emilinpoctu@gmail.com>
2025-08-01 21:40:57 +0400

    aaaaa

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ Measures the runtime of running four async_comprehension coroutines concurrently
 
 import asyncio
 from time import perf_counter
-async_comprehension = __import__('1-async_comprehension').sync_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime():
     """
